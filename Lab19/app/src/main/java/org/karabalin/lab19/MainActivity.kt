06d95cef8708757726ab1e788d7c42ef261deebe
Ruslan Karabalin <karabalin112@gmail.com>
2024-11-28 18:35:34 +0600

    feat: add Lab19

---
+++ Lab19/app/src/main/java/org/karabalin/lab19/MainActivity.kt
@@ -0,0 +1,32 @@
+package org.karabalin.lab19
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.getSettings().setJavaScriptEnabled(true)
+
+        webView.loadUrl("https://duckduckgo.org")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
